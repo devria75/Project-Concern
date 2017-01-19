659b7a61d572ad555b3a7f592b5971b416c2c102
Las499 <lilukura@gmail.com>
2017-01-19 12:12:07 -0500

    Saving Stuff before mac dies

--- Project1/Assets/Scripts/CrowdGenerator.cs
+++ Project1/Assets/Scripts/CrowdGenerator.cs
@@ -2,7 +2,11 @@
 using System.Collections;
 
 public class CrowdGenerator : MonoBehaviour {
-
+	
+	public float sensorLength = 5.0f;
+	public float speed =10.0f;
+	//control where it goes
+	public float direction = 1.0f;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +14,21 @@ public class CrowdGenerator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		RaycastHit hit;
+		if(Physics.Raycast(transform.position, transform.right, out hit(sensorLength + transform.localScale.x)))
+		transform.position += transform.forward * (speed*direction) * Time.deltaTime;
+		//transform.position += transform.forward * speed * Time.deltaTime;
 	
+	}
+
+	void OnDrawGizmos(){
+
+		Gizmos.DrawRay (transform.position, transform.forward * (sensorLength + transform.localScale.z));
+		Gizmos.DrawRay (transform.position, -transform.forward * (sensorLength + transform.localScale.z));
+		Gizmos.DrawRay (transform.position, transform.right * (sensorLength + transform.localScale.x));
+		Gizmos.DrawRay (transform.position, -transform.right * (sensorLength + transform.localScale.x));
+
+
 	}
 }
