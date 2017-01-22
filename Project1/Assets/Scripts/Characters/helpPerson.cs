d7b21432bea3dd1572a1d452f40408c52f11b821
Jessica_Nebolsky <jessican310@hotmail.com>
2017-01-21 22:10:49 -0500

    Help Script
    Jessica's push of the helper script

---
+++ Project1/Assets/Scripts/Characters/helpPerson.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helpPerson : MonoBehaviour {
+
+	bool helped;
+	public Material[] materials;
+	public Renderer rend;
+	// Use this for initialization
+	void Start () {
+		helped = false;
+		rend.sharedMaterial = materials [0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnMouseDown(){
+		if (helped == false) {
+			rend.sharedMaterial = materials [1];
+			helped = true;
+		}
+	}
+}
